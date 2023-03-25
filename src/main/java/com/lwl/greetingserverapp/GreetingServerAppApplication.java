79f0755f180b1ca78d36f294eb810a0b6eba476d
javawithlwl <javawithlwl@gmail.com>
2023-03-25 22:10:22 +0530

    greeting server app

---
+++ src/main/java/com/lwl/greetingserverapp/GreetingServerAppApplication.java
@@ -0,0 +1,13 @@
+package com.lwl.greetingserverapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingServerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingServerAppApplication.class, args);
+	}
+
+}
